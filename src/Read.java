21222b0df8df6aa4b24004c3c7351d6587f3a751
rtc10 <ych861031@gmail.com>
2018-04-26 10:57:10 +0800

    read file

--- src/Read.java
+++ src/Read.java
@@ -1,6 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class Read {
 
-    public static void main(String[] args){
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileReader = new FileReader("/Users/rtc10/IdeaProjects/Assembler/src/input.txt");
+
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        while(bufferedReader.ready()){
+            System.out.println(bufferedReader.readLine());
+        }
+        fileReader.close();
     }
+
 }
