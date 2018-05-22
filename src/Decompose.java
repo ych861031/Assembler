5f792c5684d684ffe1268c7f1ec6cc5ff6e1b333
ych861031 <ych861031>
2018-05-22 13:50:22 +0800

    Lisfile output

--- src/Decompose.java
+++ src/Decompose.java
@@ -101,13 +101,13 @@ public class Decompose {
 //            }
 //        }
 ////
-//        System.out.println("Look Operation...");
-//        for (int k=0;k<Operation.length;k++){
-//            if (Operation[k]!=null){
-//                System.out.println(Operation[k]);
-//            }
-//
-//        }
+        System.out.println("Look Operation...");
+        for (int k=0;k<Operation.length;k++){
+            if (Operation[k]!=null){
+                System.out.println(Operation[k]);
+            }
+
+        }
 
 //        System.out.println("Look Operend...");
 //        for (int k=0;k<Operend.length;k++){
--- src/output_LISFILE.java
+++ src/output_LISFILE.java
@@ -1,6 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class output_LISFILE {
 
-    public static void  output(){
+    public static void  output()throws IOException {
         //可以拿到location
         //Loc.locTest;
 
@@ -13,5 +16,43 @@ public class output_LISFILE {
         //ex:Ｆile ... = new file;
         //檔案輸出的形式可以看看assembler exercise 的LISFILE
         //先把這個檔案的ＳＲＣＦＩＬＥ.txt的檔案內容複製到ＳＲＣＦＩＬＥ裡 執行sicasm.exe 就可以開ＬＩＳＦＩＬＥ看結果
+
+        FileWriter fr = new FileWriter("LISFILE.txt");
+        for (int i=0;i<Loc.locTest.length;i++){
+
+            //opcode 格式調整
+            String opcode;
+            if (!GenerateOpcode.Opcode_test[i].equals("")){
+                opcode = GenerateOpcode.Opcode_test[i];
+                for (int space = GenerateOpcode.Opcode_test[i].length(); space <= 8 ;space++){
+                    opcode += " ";
+                }
+            }else{
+                opcode = "         ";//" "*8
+            }
+
+            String operation = "";
+            if (Decompose.Operation[i]==null){
+                operation = "      ";//" "*8
+            }else{
+                operation = Decompose.Operation[i];
+                if (Decompose.Operation[i].length()<6){
+                    for (int space = Decompose.Operation[i].length();space<=6;space++){
+                        operation+=" ";
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+            fr.write(Loc.locTest[i]+"   "  + opcode + "   " + operation +"\n");
+        }
+        fr.flush();
+        fr.close();
     }
 }
